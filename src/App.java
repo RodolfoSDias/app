5413dbdbf5fb0dd2d8ae2bacc35d42cd834f7504
RodolfoSDias <rodolfodasilvadias@outlook.com>
2023-08-09 08:48:09 -0300

    aula java

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      double x = 10.35784;
+        System.out.println(x);
+        System.out.printf("%.2f%n", x);
+    }
+}
